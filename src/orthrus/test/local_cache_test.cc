61f1fc40a114b79db6cd997d1fb40cad1d2d54fc
Vicente Adolfo Bolea Sanchez <vicente@unist.ac.kr>
2014-03-19 02:10:51 +0900

    Local cache compiles

--- src/orthrus/test/local_cache_test.cc
+++ src/orthrus/test/local_cache_test.cc
@@ -11,7 +11,7 @@ struct fix_local_cache {
   success = 0;
   failed = 0;
   cache = new Local_cache ();
-  cache .set_policy (CACHE_SPATIAL) .set_size (SIZE);
+  (*cache) .set_policy (orthrus::SPATIAL) .set_size (SIZE);
  }
  ~fix_local_cache () {
   delete cache;
@@ -24,10 +24,11 @@ SUITE (LOCAL_CACHE) {
  //--------------------------------------//
  TEST_FIXTURE (fix_local_cache, insert) {
   const uint64_t left = 100, right = 200, size = 10;
-  disk_page_t dp .set_index (50) .set_size (size) .set_data ("WHASAPGUYS");
+  disk_page_t dp;
+  dp .set_index (50) .set_size (size) .set_data ("WHASAPGUYS");
 
-  boundaries_update (100, 200);
-  insert (50, dp);
+  cache->boundaries_update (100, 200);
+  cache->insert (50, dp);
  }
 
  //--------------------------------------//
