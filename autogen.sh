36e7c3e2e5b6d12f7f11aa02a7ab6e08151d96d6
Vicente Adolfo Bolea Sanchez <vicente.bolea@gmail.com>
2015-04-01 17:09:28 +0900

    ready for merging

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 
 [ ! -e .autotools_aux ] && mkdir .autotools_aux
 
-aclocal -I m4
-autoconf
-autoheader
-automake --add-missing --copy
+aclocal -I m4                     # Generate aclocal
+autoconf                          # Generate configure script 
+autoheader                        # Generate config.h
+automake --add-missing --copy     # Generate Makefile.in and other scripts 
