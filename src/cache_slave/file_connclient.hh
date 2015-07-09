c51f9b36548e52d4b011e0d1a44624c55c090018
Vicente Adolfo Bolea Sanchez <vicente.bolea@gmail.com>
2015-07-09 21:04:05 +0900

    organized directory structure, fixed some bugs

--- src/cache_slave/file_connclient.hh
+++ src/cache_slave/file_connclient.hh
@@ -5,11 +5,11 @@
 #include <fstream>
 #include <fcntl.h>
 #include <common/dataentry.hh>
-#include <file_distributor/filepeer.hh>
+#include <common/ecfs.hh>
+#include "filepeer.hh"
 #include "messagebuffer.hh"
 #include "idistributor.hh"
 #include "writecount.hh"
-#include <common/ecfs.hh>
 
 using namespace std;
 
